431b7a6175d0f9a4b82fccd79c34629c1dace178
Valentin Richard <vrichard@mandint.org>
2019-06-18 13:25:50 +0200

    update mapNotificationsPipe

---
+++ src/operators/pipes/aggregateNotificationsPipe.ts
@@ -0,0 +1,24 @@
+import { IPipe, TPipeContextBase } from '../../core/observable-observer/interfaces';
+import { IObserver } from '../../core/observer/interfaces';
+import { Pipe } from '../../core/observable-observer/implementation';
+import { INotification } from '../../notifications/core/notification/interfaces';
+import { IObservable } from '../../core/observable/interfaces';
+
+/**
+ * ObservableObserver: aggregates many notifications by filtering their name - simple return the received values
+ *  - when a notification is received, the pipe emits the notifications's value if the notification's name is in inNames
+ * @param inNames
+ */
+export function aggregateNotifications<TKeysIn extends string, TValue>(inNames: Iterable<TKeysIn>): IPipe<IObserver<INotification<TKeysIn, TValue>>, IObservable<TValue>> {
+  type TNotification = INotification<TKeysIn, TValue>;
+  return Pipe.create<TNotification, TValue>((context: TPipeContextBase<TNotification, TValue>) => {
+    const inNamesSet: Set<TKeysIn> = new Set<TKeysIn>(inNames);
+    return {
+      onEmit(notification: TNotification): void {
+        if (inNamesSet.has(notification.name)) {
+          context.emit(notification.value);
+        }
+      }
+    };
+  });
+}
