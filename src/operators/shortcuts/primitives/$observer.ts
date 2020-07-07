cb6bb24e023638682cb3637db767e7b1f1d0f430
Valentin Richard <contact@valentin-richard.com>
2020-07-07 10:40:52 +0200

    add new parameter 'activateMode' mode to $observer, in the case when want to auto activate a callback

--- src/operators/shortcuts/primitives/$observer.ts
+++ src/operators/shortcuts/primitives/$observer.ts
@@ -1,13 +1,48 @@
 import { IObserver } from '../../../core/observer/interfaces';
 import { IsObserver } from '../../../core/observer/constructor';
-import { Observer } from '../../../core/observer/implementation';
 import { TObserverOrCallback } from '../../../core/observable/types';
+import { Observer } from '../../../core/observer/public';
+
+export type TObserverOrCallbackToObserverActivateMode =
+  'none' // (default) no specific action
+  | 'activate' // activate the observer
+  | 'deactivate' // deactivate the observer
+  | 'activate-if-callback' // activate the observer, only if a callback was provided
+  ;
 
 /**
  * Converts a callback or an Observer to an Observer
  */
-export function $observer<T>(input: TObserverOrCallback<T>): IObserver<T> {
-  return IsObserver(input)
-    ? input
+export function $observer<T>(
+  input: TObserverOrCallback<T>,
+  activateMode: TObserverOrCallbackToObserverActivateMode = 'none',
+): IObserver<T> {
+  const isObserver: boolean = IsObserver(input);
+  const observer: IObserver<T> = isObserver
+    ? input as IObserver<T>
     : new Observer<T>(input as (value: T) => void);
+
+  switch (activateMode) {
+    case 'none':
+      break;
+    case 'activate':
+      observer.activate();
+      break;
+    case 'deactivate':
+      observer.deactivate();
+      break;
+    case 'activate-if-callback':
+      if (!isObserver) {
+        observer.activate();
+      }
+      break;
+    default:
+      throw new TypeError(`Unknown activateMode '${ activateMode }'`)
+  }
+
+  return observer;
+
+  // return IsObserver(input)
+  //   ? input
+  //   : new Observer<T>(input as (value: T) => void);
 }
